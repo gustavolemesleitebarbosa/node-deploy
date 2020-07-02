72a50fc4e3811777ff586e87e8f7211f74338d7d
Gustavo <pregustavomium@gmail.com>
2020-07-02 18:50:55 -0300

    turned caching back

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -14,7 +14,7 @@ export default {
     redis: {
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
-      paswword: process.env.REDIS_PASS || undefined,
+      password: process.env.REDIS_PASS || undefined,
     },
   },
 } as ICacheConfig;
